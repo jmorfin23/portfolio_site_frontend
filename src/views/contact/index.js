944b699c9c21d3406c3badd41d28a04a37e6ab27
Jonathan Morfin <jmorfin776@gmail.com>
2019-11-14 13:25:29 -0500

    contact form started

---
+++ src/views/contact/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.css';
+
+
+class Contact extends Component {
+  render() {
+  return (
+    <div className="contact">
+      
+    </div>
+  );
+}
+}
+export default Contact;
